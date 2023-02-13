6c83a4ef8b5e37d2e6dd55f14414d638268c8405
josue <josue25971@gmail.com>
2023-02-12 23:49:52 -0600

    Ejercicios de tarea introWeb7

---
+++ introWeb7/ejercicios_Intro_Web/js/ejercicio1.js
@@ -0,0 +1,6 @@
+function cambioNombre(){
+    
+    let nombre = prompt("Cual es tu nombre: ");
+    alert("Tu nombre fue cambiado");
+    document.write("Hola " + nombre);
+}
---
+++ introWeb7/ejercicios_Intro_Web/js/ejercicio2.js
@@ -0,0 +1,8 @@
+function editSection1(){
+    let edit=prompt("Escribe lo que vas a agreage en la seccion 1: ");
+    document.getElementById("section1").innerHTML+= " "  + edit
+}
+function editSection2(){
+    let edit=prompt("Escribe lo que vas a agreage en la seccion 2: ");
+    document.getElementById("section2").innerHTML+= " " + edit
+}
